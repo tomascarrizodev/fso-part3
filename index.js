37089c422e309ef0b85906697e63280608df24fd
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2024-01-22 21:39:21 -0300

    part3 exercises 3.1-3.8

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('person', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+const persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person)
+    res.json(person)
+  else
+    res.status(404).end()
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} person<p><p>${Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+  const id = Math.floor(Math.random() * 10000)
+  person.id = id
+  let unique = true
+  const condition = [...persons].forEach(pers => {
+    if (pers.name === person.name) {
+      unique = false
+      return false
+    }
+  })
+
+  if (person.name && person.number && unique) {
+    res.json(person)
+  } else if (!unique) {
+    return res.status(204).json({ "error": "name must be unique" })
+  } else if (person.name === '' || person.number === '') {
+    return res.status(204).json({ "error": "fields must not be blank" })
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT)
+})
