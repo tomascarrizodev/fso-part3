5328948eb5825f943238d7f35ac940ac90e857b0
Tomas Carrizo <tomascarrizo.dev@gmail.com>
2024-01-23 19:19:02 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.json())
@@ -10,75 +13,71 @@ app.use(express.static('dist'))
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+// let persons = [
+//   {
+//     "id": 1,
+//     "name": "Arto Hellas",
+//     "number": "040-123456"
+//   },
+//   {
+//     "id": 2,
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523"
+//   },
+//   {
+//     "id": 3,
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345"
+//   },
+//   {
+//     "id": 4,
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122"
+//   }
+// ]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
 app.get('/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person)
+  Person.findById(req.params.id).then(person => {
     res.json(person)
-  else
-    res.status(404).end()
+  })
 })
 
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} person<p><p>${Date()}</p>`)
 })
 
-app.delete('/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
-})
+// app.delete('/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
+//   persons = persons.filter(person => person.id !== id)
+//   res.status(204).end()
+// })
 
 app.post('/persons', (req, res) => {
-  const person = req.body
-  const id = Math.floor(Math.random() * 10000)
-  person.id = person.id || id
-  let unique = true
-  const condition = [...persons].forEach(pers => {
-    if (pers.name === person.name) {
-      unique = false
-      return false
-    }
+  const body = req.body
+
+  if (body.name == false) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+
+  const person = new Person({
+    id: body.id,
+    name: body.name,
+    number: body.number,
   })
 
-  if (person.name && person.number && unique) {
-    res.json(person)
-  } else if (!unique) {
-    return res.status(204).json({ "error": "name must be unique" })
-  } else if (person.name === '' || person.number === '') {
-    return res.status(204).json({ "error": "fields must not be blank" })
-  }
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT || 3001
